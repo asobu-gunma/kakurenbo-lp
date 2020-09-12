1c8849dcfab204503311738797ae574e8b84efea
Yuta Toyokawa <kppg42@gmail.com>
2020-09-12 11:31:36 +0900

    スマホ用に告知を切り替え

--- components/parts/eventButton.vue
+++ components/parts/eventButton.vue
@@ -7,8 +7,10 @@ n-link.event-button(
   to
 )
   .event-detail
-    .event-date {{ eventButtonDate }}開催！
-    .event-enter 詳細はコチラ！
+    img.event-image(v-lazy="event.fields.image.fields.file.url")
+    .event-text
+      .event-date {{ eventButtonDate }}開催！
+      .event-enter 詳細はコチラ！
 </template>
 
 <script>
@@ -31,34 +33,50 @@ export default {
 
 <style lang="sass" scoped>
 .event-button
-  background-size: cover
-  background-position: center center
-  border: 5px $accent-color solid
-  border-radius: 50%
   position: fixed
-  overflow: hidden
   z-index: 100
   @include media-breakpoint-up(sm)
+    background-size: cover
+    background-position: center center
+    border: 5px $accent-color solid
+    border-radius: 50%
+    overflow: hidden
     width: 150px
     height: 150px
     bottom: 40px
     right: 60px
   @include media-breakpoint-down(xs)
-    display: none
-    width: 80px
-    height: 80px
-    bottom: 30px
-    right: 30px
+    display: block
+    background-image: none !important
+    background-color: $accent-color
+    width: 100%
+    padding: 5px
+    bottom: 0
   .event-detail
+    text-align: center
     @include media-breakpoint-up(sm)
       color: white
       font-size: 11px
       font-weight: bold
-      text-align: center
       width: 100%
       background-color: rgba(0, 0, 0, 0.7)
       padding: 7px 20px 15px
       position: absolute
       bottom: 0
+      .event-image
+        display: none
     @include media-breakpoint-down(xs)
+      color: $primary-grey
+      font-size: 16px
+      font-weight: bold
+      display: flex
+      align-items: center
+      .event-image
+        height: 70px
+        margin-right: 10px
+      .event-text
+        text-align: center
+        width: 100%
+        .event-date, .event-enter
+          display: inline-block
 </style>
