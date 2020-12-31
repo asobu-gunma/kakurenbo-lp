7c1731d65545c43250c0f4a6cb8750be2f9a38f3
Yuta Toyokawa <kppg42@gmail.com>
2021-01-01 07:32:25 +0900

    優勝者ページデザインのみ

--- components/sections/Champion.vue
+++ components/sections/Champion.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .champion
   .container
-    .row
+    .row.champion-list
       .col-lg-3.col-sm-4.mb-5
         .photo
         .title-area
@@ -24,32 +24,33 @@
 
 <style lang="sass" scoped>
 .champion
-  .photo
-    width: 100%
-    padding-bottom: 150%
-    background-size: cover
-    background-position: center center
-    background-color: $secondary-grey
-    border-radius: 5px
-  .title-area
-    display: flex
-    justify-content: center
-    align-items: center
-    width: 75px
-    height: 75px
-    background-color: white
-    border-radius: 50%
-    margin: -20px auto 0
-    .title
-      letter-spacing: 2px
+  .champion-list
+    .photo
+      width: 100%
+      padding-bottom: 150%
+      background-size: cover
+      background-position: center center
+      background-color: $secondary-grey
+      border-radius: 5px
+    .title-area
+      display: flex
+      justify-content: center
+      align-items: center
+      width: 75px
+      height: 75px
+      background-color: white
+      border-radius: 50%
+      margin: -20px auto 0
+      .title
+        letter-spacing: 2px
+        font-weight: bold
+        .num
+          font-size: 28px
+    .name
+      text-align: center
+      font-size: 24px
       font-weight: bold
-      .num
-        font-size: 28px
-  .name
-    text-align: center
-    font-size: 24px
-    font-weight: bold
-  .nickname
-    text-align: center
-    color: $secondary-grey
+    .nickname
+      text-align: center
+      color: $secondary-grey
 </style>
