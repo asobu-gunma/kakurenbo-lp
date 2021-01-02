d50781ac41eda455313b2e4d796746749afa52b1
Yuta Toyokawa <kppg42@gmail.com>
2021-01-03 06:30:32 +0900

    フォームデザインの実装

---
+++ components/sections/EntryForm.vue
@@ -0,0 +1,121 @@
+<template lang="pug">
+form.entry-form
+  .container
+    .row
+      .offset-lg-3.offset-md-2.col-lg-6.col-md-8
+        .user-list(v-for="(user, index) in entryForm.userList", :key="index")
+          .form-group.row
+            .col-sm-3.mt-2
+              label(:for="`user_${index}_name`") お名前
+            .col-sm-9
+              input.form-control(
+                type="text",
+                placeholder="かくれんぼ 太郎",
+                :name="`user_${index}_name`",
+                :id="`user_${index}_name`",
+                required,
+                v-model="user.name"
+              )
+          .form-group.row
+            .col-sm-3
+              label 性別
+            .col-sm-9
+              .custom-control.custom-radio.custom-control-inline
+                input.custom-control-input(
+                  type="radio",
+                  :name="`user_${index}_gender_male`",
+                  :id="`user_${index}_gender_male`",
+                  value="男性",
+                  required,
+                  v-model="user.gender"
+                )
+                label.custom-control-label(:for="`user_${index}_gender_male`") 男性
+              .custom-control.custom-radio.custom-control-inline
+                input.custom-control-input(
+                  type="radio",
+                  :name="`user_${index}_gender_female`",
+                  :id="`user_${index}_gender_female`",
+                  value="女性",
+                  required,
+                  v-model="user.gender"
+                )
+                label.custom-control-label(
+                  :for="`user_${index}_gender_female`"
+                ) 女性
+          .form-group.row
+            .col-sm-3.mt-2
+              label(:for="`user_${index}_age`") 年代
+            .col-sm-9
+              select.custom-select(
+                :name="`user_${index}_age`",
+                :id="`user_${index}_age`",
+                v-model="user.age"
+              )
+                option(value="") 年代を選択してください
+                option(value="10歳未満") 10歳未満
+                option(value="10代") 10代
+                option(value="20代") 20代
+                option(value="30代") 30代
+                option(value="40代") 40代
+                option(value="50代") 50代
+                option(value="60代以上") 60代以上
+        .form-group.row
+          .col-sm-3
+            label 参加のキッカケ
+          .col-sm-9
+            .cognition-list(
+              v-for="(cognition, index) in cognitionList",
+              :key="index"
+            )
+              .custom-control.custom-checkbox
+                input.custom-control-input(
+                  type="checkbox",
+                  :name="`user_cognition_${index}`",
+                  :id="`user_cognition_${index}`",
+                  :value="cognition",
+                  required,
+                  v-model="entryForm.cognition"
+                )
+                label.custom-control-label(:for="`user_cognition_${index}`") {{ cognition }}
+        .form-group.row
+          .col-sm-3.mt-2
+            label(:for="`user_note`") 質問・要望
+          .col-sm-9
+            textarea.form-control(
+              :name="`user_note`",
+              :id="`user_note`",
+              v-model="entryForm.note"
+            )
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      entryForm: {
+        userList: [
+          {
+            name: "",
+            gender: "男性",
+            age: "",
+          },
+        ],
+        cognition: [],
+        note: "",
+      },
+      cognitionList: [
+        "運営者と知り合い",
+        "参加者からの紹介",
+        "公式サイト",
+        "公式ブログ",
+        "Facebookページ",
+        "新聞・メディア",
+        "その他",
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
---
+++ pages/entries/index.vue
@@ -0,0 +1,48 @@
+
+<template lang="pug">
+#wrapper
+  p-blog-header
+  section.section
+    p-blog-title
+    s-entry-form
+  g-footer
+</template>
+
+<script>
+import GFooter from "@/components/global/Footer";
+import PBlogHeader from "@/components/parts/BlogHeader";
+import PBlogTitle from "@/components/parts/BlogTitle";
+import SEntryForm from "@/components/sections/EntryForm";
+
+export default {
+  components: {
+    GFooter,
+    PBlogHeader,
+    PBlogTitle,
+    SEntryForm,
+  },
+  data() {
+    return {};
+  },
+  async asyncData({ route, app, env }) {},
+  head() {
+    return {
+      title: `${process.env.projectName} Official Blog`,
+      meta: [
+        {
+          name: "description",
+          hid: "description",
+          content:
+            "群馬県でかくれんぼの楽しさを広めるために活動している団体の公式ブログ。老若男女誰でも楽しめる、そんなかくれんぼのイベント情報を発信しています。",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="sass">
+#wrapper
+  .section
+    padding-bottom: 100px
+</style>
