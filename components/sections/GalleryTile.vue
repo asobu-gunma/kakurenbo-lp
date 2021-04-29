93f36c2656d9239128eba63cde6d69f03f56a71c
Yuta Toyokawa <kppg42@gmail.com>
2021-04-30 02:10:41 +0900

    タイルデザインの修正

--- components/sections/GalleryTile.vue
+++ components/sections/GalleryTile.vue
@@ -2,7 +2,10 @@
 <template lang="pug">
 #gallery-tile
   .row.no-gutters
-    .col-sm-4(v-for="(photo, index) in gallery", :key="index")
+    .col-sm-4.px-1.pb-sm-2.pb-1(
+      v-for="(photo, index) in gallery",
+      :key="index"
+    )
       .photo(v-lazy:background-image="photo.fields.file.url")
 </template>
 
@@ -24,5 +27,4 @@ export default {
     padding-top: 60%
     background-size: cover
     background-position: center center
-    margin-bottom: 15px
 </style>
